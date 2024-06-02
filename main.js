6a6fecda99a37185f3e27879fe8b140725906894
Beby Kurniawan <bebykurniawan2006@gmail.com>
2024-06-02 08:27:26 +0700

    feat: modified form and add modal pop up

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const inputBook = document.getElementById("inputBook");
-  const inCompleteBookList = document.querySelector("#incompleteBookshelfList > .book_item");
-  const completeBookList = document.querySelector("#completeBookshelfList > .book_item");
-  const bookSubmitSpan = document.querySelector('#bookSubmit > span')
+  const inCompleteBookList = document.querySelector("#incompleteBookshelfList");
+  const completeBookList = document.querySelector("#completeBookshelfList");
+  const bookSubmitSpan = document.querySelector("#bookSubmit > span");
   const inputBookIsComplete = document.getElementById("inputBookIsComplete");
+  const modalPopUp = document.getElementById("modalPopUp");
 
-  
-  //   ---> Mengubah inner text ketika user mengeklik tombol checkbox 
-  inputBookIsComplete.addEventListener('change', function() {
+  //   <---- Mengubah inner text ketika user mengeklik tombol checkbox ---->
+  inputBookIsComplete.addEventListener("change", function () {
     if (inputBookIsComplete.checked) {
       bookSubmitSpan.innerText = "Yang sudah selesai dibaca";
     } else {
       bookSubmitSpan.innerText = "Belum selesai dibaca";
     }
   });
-  
-  //   ---> Submit Form Buku
+
+  //   <--- Submit Form Buku --->
   inputBook.addEventListener("submit", function (event) {
     event.preventDefault();
     const title = document.getElementById("inputBookTitle").value;
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const year = document.getElementById("inputBookYear").value;
     const isComplete = document.getElementById("inputBookIsComplete").checked;
 
-    //   ---> Object buku
+    //   <--- Object buku --->
     const book = {
       id: +new Date(),
       title: title,
@@ -31,25 +31,45 @@ document.addEventListener("DOMContentLoaded", function () {
       year: parseInt(year),
       isComplete: isComplete,
     };
-      isComplete ? addCompleteBooks(book) : addInCompleteBooks(book);
+    isComplete ? addCompleteBooks(book) : addInCompleteBooks(book);
+
+    //   <--- Mengkosongkan input fields setelah user submit --->
+    document.getElementById("inputBookTitle").value = "";
+    document.getElementById("inputBookAuthor").value = "";
+    document.getElementById("inputBookYear").value = "";
+    document.getElementById("inputBookIsComplete").checked = false;
+
+    modalPopUp.classList.add('show')
+    setTimeout(() => {
+      modalPopUp.classList.remove('show')
+    }, 3000);
   });
-  //   ---> Function Tambah Buku belum dibaca
+
+  //   <--- Function Tambah Buku belum dibaca --->
   const addInCompleteBooks = ({ id, title, author, year, isComplete }) => {
-    const bookItem = document.createElement("div");
+    const bookItem = document.createElement("article");
     bookItem.classList.add("book_item");
     bookItem.innerHTML = `<h3>${title}</h3>
     <p>Penulis: ${author}</p>
-    <p>TahunL: ${year}</p>`;
+    <p>Tahun: ${year}</p>
+    <div class="action">
+    <button class="green">Selesai dibaca</button>
+    <button class="red">Hapus buku</button>
+    </div>`;
     inCompleteBookList.appendChild(bookItem);
   };
 
-  //   ---> Function Tambah Buku yang sudah dibaca
+  //   <--- Function Tambah Buku yang sudah dibaca --->
   const addCompleteBooks = ({ id, title, author, year, isComplete }) => {
-    const bookItem = document.createElement("div");
+    const bookItem = document.createElement("article");
     bookItem.classList.add("book_item");
-    bookItem.innerHTML = `<h3>${title}</h3><p>${author}</p><p>${year}</p>`;
-    completeBookList.appendChild(bookItem);
+    bookItem.innerHTML = `<h3>${title}</h3>
+    <p>Penulis: ${author}</p>
+    <p>Tahun: ${year}</p>
+    <div class="action">
+    <button class="green">Selesai dibaca</button>
+    <button class="red">Hapus buku</button>
+    </div>`;
+    completeBookList.append(bookItem);
   };
 });
-
-// Pr besok : mmebuat 
